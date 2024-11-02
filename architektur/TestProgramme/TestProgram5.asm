5ff49ff0cf0c485cdfd9950aaeb4d38ad4106927
Lukas Reil <lukas_reil0305@gmx.net>
2024-11-02 14:08:02 +0100

    Modified TestProgram5 to load an image from RAM

--- architektur/TestProgramme/TestProgram5.asm
+++ architektur/TestProgramme/TestProgram5.asm
@@ -1,10 +1,9 @@
 IML  $t0, 0
 IMH  $t0, 0
 ALU  0, $t0, $t0 
-IML  $t9, 0xff
-IMH  $t9, 0x0f
 IML  $t0, 1
 IMH  $t0, 0
-ALU  0, $AO, $t0
+RDMr $t9, $AO
 EXT  1, $AO, $t9
-ja 6
+ALU  0, $AO, $t0
+ja 5
