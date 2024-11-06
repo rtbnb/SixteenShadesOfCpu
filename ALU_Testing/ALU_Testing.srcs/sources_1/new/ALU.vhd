0d36413b3bf57f61140635aada928af099a799b7
Matthiaszockt <54158205+Matthiaszockt@users.noreply.github.com>
2024-11-06 10:22:34 +0100

    set up alu testing project and add entity

---
+++ ALU_Testing/ALU_Testing.srcs/sources_1/new/ALU.vhd
@@ -0,0 +1,49 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 06.11.2024 10:18:06
+-- Design Name: 
+-- Module Name: ALU - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity ALU is
+ Port ( 
+    D1: IN std_logic_vector(15 downto 0 );
+    D2: IN std_logic_vector(15 downto 0 );
+    ALU_OPP: IN std_logic_vector(3 downto 0 );
+    ALU_OUT: OUT std_logic_vector(15 downto 0 );
+    ALU_FLAGS: in std_logic_vector(15 downto 0 )
+ );
+end ALU;
+
+architecture Behavioral of ALU is
+
+begin
+
+
+end Behavioral;
