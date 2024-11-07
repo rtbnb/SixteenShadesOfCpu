c3f34d91a39d1d96b3e20e84582d7c6cbf1eddab
NavisOnAir <nico.tunkowski@gmail.com>
2024-11-07 09:59:03 +0100

    RHOPin configuration

--- vivado/RegisterFile/RegFile.runs/synth_1/runme.sh
+++ vivado/RegisterFile/RegFile.runs/synth_1/runme.sh
@@ -25,7 +25,7 @@ else
 fi
 export LD_LIBRARY_PATH
 
-HD_PWD='C:/Development/SixteenShadesOfCpu/RegisterFile/RegFile.runs/synth_1'
+HD_PWD='C:/Development/SixteenShadesOfCpu/vivado/RegisterFile/RegFile.runs/synth_1'
 cd "$HD_PWD"
 
 HD_LOG=runme.log
