3039757cd0e7300ad51bfe5c08ba5c12b64233f1
Lukas Reil <lukas_reil0305@gmx.net>
2024-11-07 13:52:59 +0100

    Fixed errors in Decoder and implemented Jump Controller with reference to Flag Unpacker

--- vivado/ControlUnit/ControlUnit.srcs/sources_1/new/CU_Decoder.vhd
+++ vivado/ControlUnit/ControlUnit.srcs/sources_1/new/CU_Decoder.vhd
@@ -37,12 +37,12 @@ entity CU_Decoder is
            Reg2Read : out STD_LOGIC;
            RF_WHB : out STD_LOGIC;
            RF_WLB : out STD_LOGIC;
-           Write_Data_Sel : out STD_LOGIC;
+           Write_Data_Sel : out STD_LOGIC_VECTOR (1 downto 0);
            RAM_Src : out STD_LOGIC;
            RAM_Read : out STD_LOGIC;
            RAM_Write : out STD_LOGIC;
            JMP : out STD_LOGIC;
-           JMP_Condintional : out STD_LOGIC;
+           JMP_Conditional : out STD_LOGIC;
            JMP_Relative : out STD_LOGIC;
            Is_ALU_OP : out STD_LOGIC;
            Is_RAM_OP : out STD_LOGIC);
@@ -50,22 +50,22 @@ end CU_Decoder;
 
 architecture Behavioral of CU_Decoder is
     signal instruction_name : STD_LOGIC_VECTOR (3 downto 0);
-    signal is_nop, is_alu, is_rdmi, is_wrmi, is_iml, is_imh, is_rdmr, is_wrmr, is_jc, is_jr, is_ja, is_cr : STD_LOGIC;
-    signal write_whole_bytes : STD_LOGIC;
+    signal is_nop, is_alu, is_rdmi, is_wrmi, is_iml, is_imh, is_rdmr, is_wrmr, is_jc, is_jr, is_ja, is_cr : boolean;
+    signal write_whole_byte : boolean;
 begin
     instruction_name <= Instruction(15 downto 12);
-    is_nop  <= '1' WHEN instruction_name = "0000" ELSE '0';
-    is_alu  <= '1' WHEN instruction_name = "0001" ELSE '0';
-    is_rdmi <= '1' WHEN instruction_name = "0010" ELSE '0';
-    is_wrmi <= '1' WHEN instruction_name = "0011" ELSE '0';
-    is_iml  <= '1' WHEN instruction_name = "0100" ELSE '0';
-    is_imh  <= '1' WHEN instruction_name = "0101" ELSE '0';
-    is_rdmr <= '1' WHEN instruction_name = "0110" ELSE '0';
-    is_wrmr <= '1' WHEN instruction_name = "0111" ELSE '0';
-    is_jc   <= '1' WHEN instruction_name = "1000" ELSE '0';
-    is_jr   <= '1' WHEN instruction_name = "1001" ELSE '0';
-    is_ja   <= '1' WHEN instruction_name = "1010" ELSE '0';
-    is_cr   <= '1' WHEN instruction_name = "1110" ELSE '0'; 
+    is_nop  <= instruction_name = "0000";
+    is_alu  <= instruction_name = "0001";
+    is_rdmi <= instruction_name = "0010";
+    is_wrmi <= instruction_name = "0011";
+    is_iml  <= instruction_name = "0100";
+    is_imh  <= instruction_name = "0101";
+    is_rdmr <= instruction_name = "0110";
+    is_wrmr <= instruction_name = "0111";
+    is_jc   <= instruction_name = "1000";
+    is_jr   <= instruction_name = "1001";
+    is_ja   <= instruction_name = "1010";
+    is_cr   <= instruction_name = "1110"; 
     
     -- ALU for operand1
     -- WRMI for write data
@@ -73,32 +73,35 @@ begin
     -- WRMR for write data
     -- JR for jump address
     -- CR for write data
-    Reg1Read <= is_alu or is_wrmi or is_iml or is_imh or is_wrmr or is_jr or is_cr;
+    Reg1Read <= '1' WHEN is_alu or is_wrmi or is_iml or is_imh or is_wrmr or is_jr or is_cr ELSE '0';
     
     -- ALU for operand2
     -- RDMR and WRMR for address
     -- JC for flags 
-    Reg2Read <= is_alu or is_rdmr or is_wrmr or is_jc;
+    Reg2Read <= '1' WHEN is_alu or is_rdmr or is_wrmr or is_jc ELSE '0';
     
     -- ALU for writing result to $AO
     -- RDMI/RDMR for writing memory read result
     -- CR for writing to dst register
     write_whole_byte <= is_alu or is_rdmi or is_rdmr or is_cr; 
     
-    RF_WHB <= write_whole_byte or is_imh;
-    RF_WLB <= write_whole_byte or is_iml;
+    RF_WHB <= '1' WHEN write_whole_byte or is_imh ELSE '0';
+    RF_WLB <= '1' WHEN write_whole_byte or is_iml ELSE '0';
     
     -- ALU => Select 0
     -- IMH/IML => Select 1
     -- CR => Select 2
     -- RDMR/RDMI (others) => Select 3
-    Write_Data_Sel <= "00" WHEN is_alu ELSE ("01" WHEN is_imh or is_iml ELSE ("10" WHEN is_cr ELSE "11"));
+    Write_Data_Sel <= "00" WHEN is_alu ELSE 
+                      "01" WHEN is_imh or is_iml ELSE 
+                      "10" WHEN is_cr ELSE 
+                      "11";
     
-    JMP <= is_jc or is_jr or is_ja;
-    JMP_Conditional <= is_ic;
-    JMP_Relative <= is_jc;
+    JMP <= '1' WHEN is_jc or is_jr or is_ja ELSE '0';
+    JMP_Conditional <= '1' WHEN is_jc ELSE '0';
+    JMP_Relative <= '1' WHEN is_jc ELSE '0';
     
-    Is_ALU_OP <= is_alu;
-    Is_RAM_OP <= is_rdmi or is_wrmi or is_rdmr or is_wrmr;
+    Is_ALU_OP <= '1' WHEN is_alu ELSE '0';
+    Is_RAM_OP <= '1' WHEN is_rdmi or is_wrmi or is_rdmr or is_wrmr ELSE '0';
     
 end Behavioral;
