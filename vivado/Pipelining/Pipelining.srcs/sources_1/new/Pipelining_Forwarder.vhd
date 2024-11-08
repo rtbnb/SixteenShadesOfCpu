4044caa29b5c2edc3413322a06bdab83bfdf7478
Lukas Reil <lukas_reil0305@gmx.net>
2024-11-08 18:17:35 +0100

    Implemented the Pipelining Forwarder

--- vivado/Pipelining/Pipelining.srcs/sources_1/new/Pipelining_Forwarder.vhd
+++ vivado/Pipelining/Pipelining.srcs/sources_1/new/Pipelining_Forwarder.vhd
@@ -44,8 +44,28 @@ entity Pipelining_Forwarder is
 end Pipelining_Forwarder;
 
 architecture Behavioral of Pipelining_Forwarder is
-
+    signal operand_1_sel_s, operand_2_sel_s : STD_LOGIC_VECTOR(1 downto 0);
+    signal ma_sel_s : STD_LOGIC; 
 begin
-
+    
+    operand_1_sel_s <= ForwardingConfiguration(1 downto 0);
+    operand_2_sel_s <= ForwardingConfiguration(3 downto 2);
+    ma_sel_s <= ForwardingConfiguration(4);
+    
+    WITH (operand_1_sel_s) SELECT ForwardedOperand1 <=
+        CurrentOperand1 WHEN "00",
+        CurrentOperand1 WHEN "01",
+        ExecutionWriteData WHEN "10",
+        ExecutionFlags WHEN "11";
+        
+    WITH (operand_2_sel_s) SELECT ForwardedOperand2 <=
+        CurrentOperand2 WHEN "00",
+        CurrentOperand2 WHEN "01",
+        ExecutionWriteData WHEN "10",
+        ExecutionFlags WHEN "11";
+        
+    WITH (ma_sel_s) SELECT ForwardedMA <=
+        CurrentMA WHEN '0',
+        ExecutionWriteData WHEN '1';
 
 end Behavioral;
