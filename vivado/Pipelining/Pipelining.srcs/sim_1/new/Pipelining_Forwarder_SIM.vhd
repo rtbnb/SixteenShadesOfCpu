db92ee30c7c4d19239c1abcd31f9a301b719af9f
LukasReil <lukas_reil0305@gmx.net>
2024-11-12 18:33:05 +0100

    Finalized and Simulated Pipeling WriteBackStage

---
+++ vivado/Pipelining/Pipelining.srcs/sim_1/new/Pipelining_Forwarder_SIM.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 12.11.2024 17:21:33
+-- Design Name: 
+-- Module Name: Pipelining_Forwarder_SIM - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Pipelining_Forwarder_SIM is
+--  Port ( );
+end Pipelining_Forwarder_SIM;
+
+architecture Behavioral of Pipelining_Forwarder_SIM is
+
+begin
+
+
+end Behavioral;
