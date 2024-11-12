693e9fbfb6212167b3f0492ab0a62b000068a23c
rtbnb <eilers.robin@mail.de>
2024-11-12 23:06:58 +0100

    gram working on new clocking principle

--- vivado/memory_tryout/memory_tryout.gen/sources_1/bd/main_block/hdl/main_block_wrapper.vhd
+++ vivado/memory_tryout/memory_tryout.gen/sources_1/bd/main_block/hdl/main_block_wrapper.vhd
@@ -2,8 +2,8 @@
 --Copyright 2022-2024 Advanced Micro Devices, Inc. All Rights Reserved.
 ----------------------------------------------------------------------------------
 --Tool Version: Vivado v.2024.1 (win64) Build 5076996 Wed May 22 18:37:14 MDT 2024
---Date        : Tue Nov 12 14:01:02 2024
---Host        : Robin_Laptop running 64-bit major release  (build 9200)
+--Date        : Tue Nov 12 23:02:00 2024
+--Host        : DESKTOP-Q664A4O running 64-bit major release  (build 9200)
 --Command     : generate_target main_block_wrapper.bd
 --Design      : main_block_wrapper
 --Purpose     : IP block netlist
