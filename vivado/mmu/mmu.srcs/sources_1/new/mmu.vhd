2ebaadec0b5e44ddd2608e2b3812126fc4edda5a
rtbnb <eilers.robin@mail.de>
2024-11-18 13:28:26 +0100

    small changes to the code concerning the debugging

--- vivado/mmu/mmu.srcs/sources_1/new/mmu.vhd
+++ vivado/mmu/mmu.srcs/sources_1/new/mmu.vhd
@@ -84,11 +84,12 @@ entity mmu is
 end mmu;
 
 architecture Behavioral of mmu is
-    signal gram_bank_op_s, vram_bank_op_s, mmio_bank_op_s, iram_bank_op_s: std_logic;
-    signal iram_op_config_s, iram_debug_condition_s: std_logic;
-    signal iram_comb_condition_s: std_logic_vector( 1 downto 0 );
+    signal gram_comb_condition_s, vram_comb_condition_s, mmio_comb_condition_s, iram_comb_condition_s: std_logic_vector( 1 downto 0 );
     
-    signal output_config_s: std_logic_vector( 5 downto 0 );
+    signal gram_bank_op_s, vram_bank_op_s, mmio_bank_op_s, iram_bank_op_s: std_logic;
+    signal internal_debug_override_s: std_logic;
+    
+    signal output_config_s: std_logic_vector( 4 downto 0 );
     
     signal general_bank_s: std_logic_vector( 3 downto 0 );
     signal general_we_s: std_logic_vector( 0 downto 0 );
@@ -117,23 +118,26 @@ begin
         general_din_s <= debug_din when '1',
                          gram_din when others;
                         
+    internal_debug_override_s <= debug_enable and debug_override_enable;
     
-    
+    gram_bank_op_s <= not (general_bank_s(3) or general_bank_s(2) or general_bank_s(1) or general_bank_s(0));
+    vram_bank_op_s <= (not (general_bank_s(3) or general_bank_s(2) or general_bank_s(1))) and general_bank_s(0);
+    mmio_bank_op_s <= (not (general_bank_s(3) or general_bank_s(2) or general_bank_s(0))) and general_bank_s(1);
+    iram_bank_op_s <= general_bank_s(3) and general_bank_s(2) and general_bank_s(1) and general_bank_s(0);    
 --iram begin
-    iram_debug_condition_s <= debug_enable and debug_override_enable;
-    iram_op_config_s <= iram_debug_condition_s and iram_bank_op_s;
-    iram_comb_condition_s <= iram_debug_condition_s & iram_op_config_s;
+
     
-    with iram_op_config_s select
-        iram_mem_addr <= debug_addr( 13 downto 0 ) when '1',
+    with iram_comb_condition_s select
+        iram_mem_addr <= debug_addr( 13 downto 0 ) when "11",
+                         "0000000000000" when "10",
                          iram_addr when others;
                         
-    with iram_op_config_s select
-        iram_mem_din <= debug_din when '1',
+    with iram_comb_condition_s select
+        iram_mem_din <= debug_din when "11",
                         X"0000" when others;
 
-    with iram_op_config_s select
-        iram_mem_we <= ( 0 => debug_we) when '1',
+    with iram_comb_condition_s select
+        iram_mem_we <= ( 0 => debug_we) when "11",
                        "0" when others;             
 
     with iram_comb_condition_s select
@@ -142,9 +146,8 @@ begin
                          iram_addr( 13 downto 0 ) when others;
     
     with iram_comb_condition_s select
-        iram_mem_ck <= not debug_clk200mhz when "11",
-                       '0' when "10",
-                       not clk200mhz when others;
+        iram_mem_ck <= '0' when "10",
+                       general_clk_s when others;
                        
     with iram_comb_condition_s select
         iram_mem_we <= (0 => debug_we) when "11",
@@ -152,6 +155,7 @@ begin
                        
     with iram_comb_condition_s select
         iram_dout <= X"0000" when "11",
+                     X"0000" when "10",
                      iram_mem_dout when others;
 
 --iram end    
@@ -160,12 +164,9 @@ begin
 --TODO Build combined condition to only select gram when iram is not written to
 
 
-    gram_bank_op_s <= not (general_bank_s(3) or general_bank_s(2) or general_bank_s(1) or general_bank_s(0));
-    vram_bank_op_s <= (not (general_bank_s(3) or general_bank_s(2) or general_bank_s(1))) and general_bank_s(0);
-    mmio_bank_op_s <= (not (general_bank_s(3) or general_bank_s(2) or general_bank_s(0))) and general_bank_s(1);
-    iram_bank_op_s <= general_bank_s(3) and general_bank_s(2) and general_bank_s(1) and general_bank_s(0);
 
---ck section-------------------------------------------------    
+
+--ck section-------------------------------------------------
     with gram_bank_op_s select
         gram_mem_ck <= general_clk_s when '1',
                        '0' when others;
@@ -213,15 +214,23 @@ begin
                         X"0000" when others;             
 -------------------------------------------------------------
    
-    with output_config_s select
-        gram_dout <= gram_mem_dout when "000000",
-                     vrama_mem_dout when "000100",
-                     mmio_mem_dout when "001000",
+    with output_config_s select --TODO This needs to be fixed to also work with the general output
+        gram_dout <= gram_mem_dout when "00000",
+                     gram_mem_dout when "00001",
+                     vrama_mem_dout when "00010",
+                     vrama_mem_dout when "00011",
+                     mmio_mem_dout when "00100",
+                     mmio_mem_dout when "00101",
                      X"0000" when others;
+    
     with output_config_s select
-        debug_dout <= gram_mem_dout when "000010",
-                      vrama_mem_dout when "000110",
-                      mmio_mem_dout when "001010",
+        debug_dout <= gram_mem_dout when "00000",
+                      gram_mem_dout when "00001",
+                      vrama_mem_dout when "00010",
+                      vrama_mem_dout when "00011",
+                      mmio_mem_dout when "00100",
+                      mmio_mem_dout when "00101",
+                      iram_mem_dout when "11111",
                       X"0000" when others;                     
 --gram end
 
@@ -233,19 +242,15 @@ begin
     vram_dout <= vramb_mem_dout;
 --vram end
 
---debug begin
-
---debug end
-
 
 ------------------------------------------------------------
 -- This Process is only affected by the normal gram config
 -- The process doesnt do anything when debug is enabled to not deactivate debug when data is written
 --
 ------------------------------------------------------------
-    gram_lock:process(internal_clk_s)
+    gram_lock:process(clk200mhz)
     begin
-        if rising_edge(internal_clk_s) then
+        if falling_edge(clk200mhz) then
             gram_lock_s <= gram_we and (not gram_clk);
         end if;
     end process;
@@ -258,7 +263,7 @@ begin
     bank_update:process(gram_lock_s)
     begin
         if falling_edge(gram_lock_s) then
-            output_gram_bank_s <= comb_bank_s;
+            output_config_s <= general_bank_s & internal_debug_override_s;
         end if;
     end process;
 
