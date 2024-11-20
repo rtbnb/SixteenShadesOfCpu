bf50f8cb29375263e3a2075afcdfdc2422dd9c1e
Nico <nico@tunkowski.com>
2024-11-20 22:42:27 +0100

    Added Debugger GUI and multithreaded uart com

--- debug/Host/src/main.py
+++ debug/Host/src/main.py
@@ -1,24 +1,47 @@
 import serial
 import threading
+import Datapool
+
+from PySide6 import QtCore, QtWidgets, QtGui
+import sys
+from DebugWindow import DebugWindow
 
 
-def process_command(rx_data):
-    print(f"rx: {rx_data}")
+rx_data_datapool: Datapool.Datapool = Datapool.Datapool()
 
 
-def rx(ser: serial.Serial):
+def process_command(rx_data: bytes, byte_counter, command_instruction: bytes, debugWindow: DebugWindow):
+    if byte_counter == 0:
+        command_instruction = rx_data
+    elif byte_counter == 1:
+        rx_data_datapool.set_data(command_instruction, data_1_high=rx_data)
+    elif byte_counter == 2:
+        rx_data_datapool.set_data(command_instruction, data_1_low=rx_data)
+        try:
+            rx_data_datapool.get_data(command_instruction, "data_2_high")
+        except KeyError:
+            debugWindow.update_table(rx_data_datapool)
+            return (0, b'\x00')
+    elif byte_counter == 3:
+        rx_data_datapool.set_data(command_instruction, data_2_high=rx_data)
+    elif byte_counter == 4:
+        rx_data_datapool.set_data(command_instruction, data_2_low=rx_data)
+        debugWindow.update_table(rx_data_datapool)
+        return (0, command_instruction)
+    
+    return (byte_counter + 1, command_instruction)
+
+
+
+def rx(ser: serial.Serial, debugWindow: DebugWindow):
     rx_data = 0
+    byte_counter = 0
+    command_instruction: bytes = 0
     while (ser.is_open):
         rx_data = ser.read()
-        process_command(" ".join(hex(n) for n in rx_data))
+        (byte_counter, command_instruction) = process_command(rx_data, byte_counter, command_instruction, debugWindow)
 
-
-def main():
-    fpga_serial = serial.Serial('COM9', 921600)
-    #fpga_serial.open()
-    listening_thread = threading.Thread(target=rx, args=[fpga_serial])
-    listening_thread.start()
-
+def tx(ser: serial.Serial):
     # input for debug command
     while(1):
         try:
@@ -27,12 +50,30 @@ def main():
 
             byte_command = bytes([int(command, base=16)])
             print("command: ".join(hex(n) for n in byte_command))
-            fpga_serial.write(byte_command)
+            ser.write(byte_command)
         except KeyboardInterrupt:
             break
+
+def main():
+    fpga_serial = serial.Serial('COM4', 921600)
+    #fpga_serial.open()
+
+    app = QtWidgets.QApplication([])
+    widget = DebugWindow(rx_data_datapool)
+    widget.resize(800, 600)
+    widget.show()
+
+    listening_thread = threading.Thread(target=rx, args=[fpga_serial, widget])
+    listening_thread.start()
+    tx_thread = threading.Thread(target=tx, args=[fpga_serial])
+    tx_thread.start()
+    app.exec()
+
+    
     
     fpga_serial.close()
     listening_thread.join()
+    tx_thread.join()
 
 if __name__ == '__main__':
     main()
