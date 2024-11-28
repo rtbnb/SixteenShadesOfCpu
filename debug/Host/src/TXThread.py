bd7d8df25881269efe4ad20db02dc02fbe2e4f5f
Nico <nico@tunkowski.com>
2024-11-28 22:23:16 +0100

    Fixed mmu debug out + added memory fetch

--- debug/Host/src/TXThread.py
+++ debug/Host/src/TXThread.py
@@ -28,15 +28,12 @@ class TXThread(threading.Thread):
                         readback_data = self.ser.read(5)
                         print(f"Readback: {readback_data.hex()}")
                         if len(command_list) == 5:
-                            print("5")
                             if readback_data[0:1] == command_list[0] and readback_data[1:2] == command_list[1] and readback_data[2:3] == command_list[2] and readback_data[3:4] == command_list[3] and readback_data[4:5] == command_list[4]:
                                 readback_correct = True
                         elif len(command_list) == 3:
-                            print("3")
                             if readback_data[0:1] == command_list[0] and readback_data[3:4] == command_list[1] and readback_data[4:5] == command_list[2]:
                                 readback_correct = True
                         else:
-                            print("1")
                             if readback_data[0:1] == command_list[0]:
                                 readback_correct = True
                         # increase failures
@@ -54,35 +51,51 @@ class TXThread(threading.Thread):
     def  stop_thread(self):
         self.is_running = False
 
-    def process_command(self, rx_data: bytes, byte_counter, command_instruction: bytes, debugWindow: DebugWindow):
+    def process_command(self, rx_data: bytes, byte_counter, command_instruction: bytes, debugWindow: DebugWindow, ser: serial.Serial):
         rx_data_datapool = debugWindow.get_rx_datapool()
-        if byte_counter == 0:
-            command_instruction = rx_data
-            if command_instruction == b'\x0f':
-                self.failures += 1
-                print(f"Failures: {self.failures}")
-            try:
-                rx_data_datapool.get_data(command_instruction, "data_1_high")
-            except KeyError:
+        # memory read pre fetch
+        if byte_counter == 0 and (rx_data == b'\x32' or rx_data == b'\x35' or rx_data == b'\x36' or rx_data == b'\x37'):
+            data = ser.read(2)
+            addr = ser.read(2)
+            addr = int.from_bytes(addr, byteorder='big')
+            if rx_data == b'\x32':
+                rx_data_datapool.set_iram_data(addr, data)
+            elif rx_data == b'\x35':
+                rx_data_datapool.set_vram_data(addr, data)
+            elif rx_data == b'\x36':
+                rx_data_datapool.set_gram_data(addr, data)
+            elif rx_data == b'\x37':
+                rx_data_datapool.set_mmi_data(addr, data)
+            return (0, b'\x00')
+        else:
+            # normal command processing
+            if byte_counter == 0:
+                command_instruction = rx_data
+                if command_instruction == b'\x0f':
+                    self.failures += 1
+                    print(f"Failures: {self.failures}")
+                try:
+                    rx_data_datapool.get_data(command_instruction, "data_1_high")
+                except KeyError:
+                    debugWindow.update_table(rx_data_datapool)
+                    return (0, b'\x00')
+            elif byte_counter == 1:
+                rx_data_datapool.set_data(command_instruction, data_1_high=rx_data)
+            elif byte_counter == 2:
+                rx_data_datapool.set_data(command_instruction, data_1_low=rx_data)
+                try:
+                    rx_data_datapool.get_data(command_instruction, "data_2_high")
+                except KeyError:
+                    debugWindow.update_table(rx_data_datapool)
+                    return (0, b'\x00')
+            elif byte_counter == 3:
+                rx_data_datapool.set_data(command_instruction, data_2_high=rx_data)
+            elif byte_counter == 4:
+                rx_data_datapool.set_data(command_instruction, data_2_low=rx_data)
                 debugWindow.update_table(rx_data_datapool)
-                return (0, b'\x00')
-        elif byte_counter == 1:
-            rx_data_datapool.set_data(command_instruction, data_1_high=rx_data)
-        elif byte_counter == 2:
-            rx_data_datapool.set_data(command_instruction, data_1_low=rx_data)
-            try:
-                rx_data_datapool.get_data(command_instruction, "data_2_high")
-            except KeyError:
-                debugWindow.update_table(rx_data_datapool)
-                return (0, b'\x00')
-        elif byte_counter == 3:
-            rx_data_datapool.set_data(command_instruction, data_2_high=rx_data)
-        elif byte_counter == 4:
-            rx_data_datapool.set_data(command_instruction, data_2_low=rx_data)
-            debugWindow.update_table(rx_data_datapool)
-            return (0, command_instruction)
-        
-        return (byte_counter + 1, command_instruction)
+                return (0, command_instruction)
+            
+            return (byte_counter + 1, command_instruction)
 
     def rx_no_loop(self, ser: serial.Serial, debugWindow: DebugWindow):
         rx_data = 0
@@ -93,19 +106,19 @@ class TXThread(threading.Thread):
         # fetch instruction
         rx_data = ser.read()
         print(f"Received: {format(ord(rx_data), '02x')}")
-        (byte_counter, command_instruction) = self.process_command(rx_data, byte_counter, command_instruction, debugWindow)
+        (byte_counter, command_instruction) = self.process_command(rx_data, byte_counter, command_instruction, debugWindow, ser)
         if not (byte_counter == 0):
             rx_data = ser.read()
             print(f"Received: {format(ord(rx_data), '02x')}")
-            (byte_counter, command_instruction) = self.process_command(rx_data, byte_counter, command_instruction, debugWindow)
+            (byte_counter, command_instruction) = self.process_command(rx_data, byte_counter, command_instruction, debugWindow, ser)
             rx_data = ser.read()
             print(f"Received: {format(ord(rx_data), '02x')}")
-            (byte_counter, command_instruction) = self.process_command(rx_data, byte_counter, command_instruction, debugWindow)
+            (byte_counter, command_instruction) = self.process_command(rx_data, byte_counter, command_instruction, debugWindow, ser)
         
         if not (byte_counter == 0):
             rx_data = ser.read()
             print(f"Received: {format(ord(rx_data), '02x')}")
-            (byte_counter, command_instruction) = self.process_command(rx_data, byte_counter, command_instruction, debugWindow)
+            (byte_counter, command_instruction) = self.process_command(rx_data, byte_counter, command_instruction, debugWindow, ser)
             rx_data = ser.read()
             print(f"Received: {format(ord(rx_data), '02x')}")
-            (byte_counter, command_instruction) = self.process_command(rx_data, byte_counter, command_instruction, debugWindow)
+            (byte_counter, command_instruction) = self.process_command(rx_data, byte_counter, command_instruction, debugWindow, ser)
