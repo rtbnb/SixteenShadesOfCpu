88dd2519936b9a90c33b0d250cd248caffedb29d
rtbnb <eilers.robin@mail.de>
2024-11-28 18:28:21 +0100

    decoder cleanup

--- vivado/Pipelining/Pipelining.srcs/sources_1/new/Decoder.vhd
+++ vivado/Pipelining/Pipelining.srcs/sources_1/new/Decoder.vhd
@@ -1,153 +1,134 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
 -- Create Date: 09.11.2024 13:50:37
--- Design Name: 
--- Module Name: Decoder - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
+-- Name: Robin
+-- Design Name: Decoder
+-- Module Name: clockcontroller - Behavioral
+-- Project Name: ShadeCpu-1
+-- Target Devices: Arty A7-35T Development Board
+-- Repository: https://github.com/rtbnb/SixteenShadesOfCpu
 ----------------------------------------------------------------------------------
-
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity Decoder is
-    Port ( Instruction : in STD_LOGIC_VECTOR (15 downto 0);
-           Register1 : out STD_LOGIC_VECTOR (3 downto 0);
-           Register2 : out STD_LOGIC_VECTOR (3 downto 0);
-           WriteBackRegister : out STD_LOGIC_VECTOR (3 downto 0);
-           Immediate : out STD_LOGIC_VECTOR (15 downto 0);
-           JMP_Condition : out STD_LOGIC_VECTOR (2 downto 0)
+    Port ( 
+        instruction : in std_logic_vector(15 downto 0);
+        register1 : out std_logic_vector(3 downto 0);
+        register2 : out std_logic_vector(3 downto 0);
+        writeBackRegister : out std_logic_vector(3 downto 0);
+        immediate : out std_logic_vector(15 downto 0);
+        jmpCondition : out std_logic_vector(2 downto 0)
     );
 end Decoder;
 
 architecture Behavioral of Decoder is
-    
-    signal instruction_name : STD_LOGIC_VECTOR(3 downto 0);
-    signal alu_op_immediate, eight_bit_immediate, jc_immediate, ja_immediate, gpu_immediate : STD_LOGIC_VECTOR(15 downto 0);
-
+    signal instruction_name_s : std_logic_vector(3 downto 0);
+    signal alu_op_immediate_s, eight_bit_immediate_s, jc_immediate_s, ja_immediate_s, gpu_immediate_s : std_logic_vector(15 downto 0);
 begin
-    instruction_name <= Instruction(15 downto 12);
+    instruction_name_s <= instruction(15 downto 12);
     
-    WITH (instruction_name) SELECT Register1 <=
-        X"0" WHEN X"0",
-        Instruction(7 downto 4) WHEN X"1",
-        X"0" WHEN X"2",
-        Instruction(11 downto 8) WHEN X"3",
-        Instruction(11 downto 8) WHEN X"4",
-        Instruction(11 downto 8) WHEN X"5",
-        X"0" WHEN X"6",
-        Instruction(11 downto 8) WHEN X"7",
-        X"0" WHEN X"8",
-        Instruction(11 downto 8) WHEN X"9",
-        X"0" WHEN X"a",
-        X"0" WHEN X"b",
-        X"0" WHEN X"c",
-        X"0" WHEN X"d",
-        Instruction(7 downto 4) WHEN X"e",
-        Instruction(7 downto 4) WHEN X"f",
-        X"0" WHEN OTHERS;
+    with instruction_name_s select 
+        register1 <=
+            X"0" when X"0",
+            instruction(7 downto 4) when X"1",
+            X"0" when X"2",
+            instruction(11 downto 8) when X"3",
+            instruction(11 downto 8) when X"4",
+            instruction(11 downto 8) when X"5",
+            X"0" when X"6",
+            instruction(11 downto 8) when X"7",
+            X"0" when X"8",
+            instruction(11 downto 8) when X"9",
+            X"0" when X"a",
+            X"0" when X"b",
+            X"0" when X"c",
+            X"0" when X"d",
+            instruction(7 downto 4) when X"e",
+            instruction(7 downto 4) when X"f",
+            X"0" when others;
         
-    WITH (instruction_name) SELECT Register2 <=
-        X"0" WHEN X"0",
-        Instruction(3 downto 0) WHEN X"1",
-        X"0" WHEN X"2",
-        X"0" WHEN X"3",
-        X"0" WHEN X"4",
-        X"0" WHEN X"5",
-        Instruction(7 downto 4) WHEN X"6",
-        Instruction(7 downto 4) WHEN X"7",
-        X"f" WHEN X"8",
-        X"0" WHEN X"9",
-        X"0" WHEN X"a",
-        X"0" WHEN X"b",
-        X"0" WHEN X"c",
-        X"0" WHEN X"d",
-        X"0" WHEN X"e",
-        Instruction(3 downto 0) WHEN X"f",
-        X"0" WHEN OTHERS;
+    with instruction_name_s select 
+        register2 <=
+            X"0" when X"0",
+            instruction(3 downto 0) when X"1",
+            X"0" when X"2",
+            X"0" when X"3",
+            X"0" when X"4",
+            X"0" when X"5",
+            instruction(7 downto 4) when X"6",
+            instruction(7 downto 4) when X"7",
+            X"f" when X"8",
+            X"0" when X"9",
+            X"0" when X"a",
+            X"0" when X"b",
+            X"0" when X"c",
+            X"0" when X"d",
+            X"0" when X"e",
+            instruction(3 downto 0) when X"f",
+            X"0" when others;
         
-    WITH (instruction_name) SELECT WriteBackRegister <=
-        X"0" WHEN X"0",
-        X"e" WHEN X"1",
-        Instruction(11 downto 8) WHEN X"2",
-        X"0" WHEN X"3",
-        Instruction(11 downto 8) WHEN X"4",
-        Instruction(11 downto 8) WHEN X"5",
-        Instruction(11 downto 8) WHEN X"6",
-        X"0" WHEN X"7",
-        X"0" WHEN X"8",
-        X"0" WHEN X"9",
-        X"0" WHEN X"a",
-        X"0" WHEN X"b",
-        X"0" WHEN X"c",
-        X"0" WHEN X"d",
-        Instruction(11 downto 8) WHEN X"e",
-        X"0" WHEN X"f",
-        X"0" WHEN OTHERS;
+    with instruction_name_s select 
+        writeBackRegister <=
+            X"0" when X"0",
+            X"e" when X"1",
+            instruction(11 downto 8) when X"2",
+            X"0" when X"3",
+            instruction(11 downto 8) when X"4",
+            instruction(11 downto 8) when X"5",
+            instruction(11 downto 8) when X"6",
+            X"0" when X"7",
+            X"0" when X"8",
+            X"0" when X"9",
+            X"0" when X"a",
+            X"0" when X"b",
+            X"0" when X"c",
+            X"0" when X"d",
+            instruction(11 downto 8) when X"e",
+            X"0" when X"f",
+            X"0" when others;
         
-    alu_op_immediate(3 downto 0) <= Instruction(11 downto 8);
-    alu_op_immediate(15 downto 4) <= X"000";
+    alu_op_immediate_s(3 downto 0) <= instruction(11 downto 8);
+    alu_op_immediate_s(15 downto 4) <= X"000";
     
-    eight_bit_immediate(7 downto 0) <= Instruction(7 downto 0);
-    eight_bit_immediate(15 downto 8) <= X"00";
+    eight_bit_immediate_s(7 downto 0) <= instruction(7 downto 0);
+    eight_bit_immediate_s(15 downto 8) <= X"00";
     
-    
-    jc_immediate(8 downto 0) <= Instruction(8 downto 0);
+    jc_immediate_s(8 downto 0) <= instruction(8 downto 0);
     -- Signed extension
-    jc_immediate(9) <= Instruction(8);
-    jc_immediate(10) <= Instruction(8);
-    jc_immediate(11) <= Instruction(8);
-    jc_immediate(12) <= Instruction(8);
-    jc_immediate(13) <= Instruction(8);
-    jc_immediate(14) <= Instruction(8);
-    jc_immediate(15) <= Instruction(8);
+    jc_immediate_s(9) <= instruction(8);
+    jc_immediate_s(10) <= instruction(8);
+    jc_immediate_s(11) <= instruction(8);
+    jc_immediate_s(12) <= instruction(8);
+    jc_immediate_s(13) <= instruction(8);
+    jc_immediate_s(14) <= instruction(8);
+    jc_immediate_s(15) <= instruction(8);
     
-    ja_immediate(11 downto 0) <= Instruction(11 downto 0);
-    ja_immediate(15 downto 12) <= X"0";
+    jc_immediate_s(11 downto 0) <= instruction(11 downto 0);
+    jc_immediate_s(15 downto 12) <= X"0";
              
-    gpu_immediate(3 downto 0) <= Instruction(11 downto 8);
-    gpu_immediate(15 downto 4) <= X"000";
+    gpu_immediate_s(3 downto 0) <= instruction(11 downto 8);
+    gpu_immediate_s(15 downto 4) <= X"000";
     
-    WITH (instruction_name) SELECT Immediate <=
-        X"0000" WHEN X"0",
-        alu_op_immediate WHEN X"1",
-        eight_bit_immediate WHEN X"2",
-        eight_bit_immediate WHEN X"3",
-        eight_bit_immediate WHEN X"4",
-        eight_bit_immediate WHEN X"5",
-        X"0000" WHEN X"6",
-        X"0000" WHEN X"7",
-        jc_immediate WHEN X"8",
-        X"0000" WHEN X"9",
-        ja_immediate WHEN X"a",
-        X"0000" WHEN X"b",
-        X"0000" WHEN X"c",
-        X"0000" WHEN X"d",
-        X"0000" WHEN X"e",
-        gpu_immediate WHEN X"f",
-        X"0000" WHEN OTHERS;
+    with instruction_name_s select 
+        immediate <=
+            X"0000" when X"0",
+            alu_op_immediate_s when X"1",
+            eight_bit_immediate_s when X"2",
+            eight_bit_immediate_s when X"3",
+            eight_bit_immediate_s when X"4",
+            eight_bit_immediate_s when X"5",
+            X"0000" when X"6",
+            X"0000" when X"7",
+            jc_immediate_s when X"8",
+            X"0000" when X"9",
+            ja_immediate_s when X"a",
+            X"0000" when X"b",
+            X"0000" when X"c",
+            X"0000" when X"d",
+            X"0000" when X"e",
+            gpu_immediate_s when X"f",
+            X"0000" when others;
     
-    JMP_Condition <= Instruction(11 downto 9);
-
+    jmpCondition <= instruction(11 downto 9);
 end Behavioral;
