b7fbd9efb8692b2db57d2d663024e82fda90395f
LukasReil <lukas_reil0305@gmx.net>
2024-12-04 22:19:39 +0100

    Updated Pipelining Simulations

--- vivado/Pipelining/Pipelining.srcs/sources_1/new/Pipelining_WriteBackStage.vhd
+++ vivado/Pipelining/Pipelining.srcs/sources_1/new/Pipelining_WriteBackStage.vhd
@@ -20,21 +20,17 @@ entity Pipelining_WriteBackStage is
         flags : in std_logic_vector(15 downto 0);
         whb : in std_logic;
         wlb : in std_logic;
-        ramRead: in std_logic;
         isALUOp : in std_logic;
-        jmp : in std_logic;
         writeAddressOut : out std_logic_vector(3 downto 0);
         writeDataOut : out std_logic_vector(15 downto 0);
         flagsOut : out std_logic_vector(15 downto 0);
         rfWEOut : out std_logic;
-        ramReadOut : out std_logic;
-        isALUOpOut : out std_logic;
-        jmpOut : out std_logic
+        isALUOpOut : out std_logic
     );
 end entity Pipelining_WriteBackStage;
 
 architecture Behavioral of Pipelining_WriteBackStage is
-    signal rf_we_s, ram_read_s, is_alu_op_s, jmp_s : std_logic := '0';
+    signal rf_we_s, is_alu_op_s : std_logic := '0';
     signal write_address_s : std_logic_vector(3 downto 0) := (others => '0');
     signal write_data_s, flags_s : std_logic_vector(15 downto 0) := (others => '0');
 begin
@@ -43,17 +39,13 @@ begin
     begin
         if (reset = '1') then
             rf_we_s <= '0';
-            ram_read_s <= '0';
             is_alu_op_s <= '0';
-            jmp_s <= '0';
             write_address_s <= X"0";
             write_data_s <= X"0000";
             flags_s <= X"0000";
         elsif (rising_edge(instrLoadClk)) then
             rf_we_s <= whb or wlb;
-            ram_read_s <= ramRead;
             is_alu_op_s <= isALUOp;
-            jmp_s <= jmp;
             write_address_s <= writeAddress;
             write_data_s <= writeData;
             flags_s <= flags;
@@ -64,8 +56,6 @@ begin
     writeDataOut <= write_data_s;
     flagsOut <= flags_s;
     rfWEOut <= rf_we_s;
-    ramReadOut <= ram_read_s;
     isALUOpOut <= is_alu_op_s;
-    jmpOut <= jmp_s;
 
 end architecture Behavioral;
