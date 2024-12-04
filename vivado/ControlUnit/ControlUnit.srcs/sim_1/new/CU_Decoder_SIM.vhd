16a7f65dd1d6b6a8434d940abb887591c7a163db
LukasReil <lukas_reil0305@gmx.net>
2024-12-04 08:55:30 +0100

    Updated Control Unit Simulations

--- vivado/ControlUnit/ControlUnit.srcs/sim_1/new/CU_Decoder_SIM.vhd
+++ vivado/ControlUnit/ControlUnit.srcs/sim_1/new/CU_Decoder_SIM.vhd
@@ -1,92 +1,66 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
 -- Create Date: 10.11.2024 16:23:48
--- Design Name: 
+-- Name: Lukas Reil
+-- Design Name: ShadeCpu
 -- Module Name: CU_Decoder_SIM - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
+-- Project Name: ShadeCpu-1
+-- Target Devices: Arty A7-35T Development Board
+-- Repository: https://github.com/rtbnb/SixteenShadesOfCpu
 ----------------------------------------------------------------------------------
 
 
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
+library ieee;
+use ieee.std_logic_1164.all;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
-use IEEE.NUMERIC_STD.ALL;
+use ieee.numeric_std.all;
 
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity CU_Decoder_SIM is
---  Port ( );
-end CU_Decoder_SIM;
+end entity CU_Decoder_SIM;
 
 architecture Behavioral of CU_Decoder_SIM is
     component CU_Decoder is
-        Port ( Instruction : in STD_LOGIC_VECTOR (15 downto 0);
-               Reg1Read : out STD_LOGIC;
-               Reg2Read : out STD_LOGIC;
-               RF_WHB : out STD_LOGIC;
-               RF_WLB : out STD_LOGIC;
-               Write_Data_Sel : out STD_LOGIC_VECTOR (1 downto 0);
-               RAM_Address_Src : out STD_LOGIC;
-               RAM_Read : out STD_LOGIC;
-               RAM_Write : out STD_LOGIC;
-               JMP : out STD_LOGIC;
-               JMP_Conditional : out STD_LOGIC;
-               JMP_Relative : out STD_LOGIC;
-               JMP_DestinationSource : out STD_LOGIC;
-               Is_ALU_OP : out STD_LOGIC;
-               Is_RAM_OP : out STD_LOGIC);
+        Port (
+            instructionToDecode : in std_logic_vector(15 downto 0);
+            reg1Read : out std_logic;
+            reg2Read : out std_logic;
+            rfWHB : out std_logic;
+            rfWLB : out std_logic;
+            rfWrite : out std_logic;
+            writeDataSelect : out std_logic_vector(2 downto 0);
+            flagSelect : out std_logic_vector(0 downto 0);
+            ramAddressSrc : out std_logic;
+            ramRead : out std_logic;
+            ramWrite : out std_logic;
+            jmp : out std_logic;
+            jmpConditional : out std_logic;
+            jmpRelative : out std_logic;
+            jmpDestinationSource : out std_logic;
+            isALUOp : out std_logic;
+            isRAMOp : out std_logic;
+            isGPUOp : out std_logic
+        );
     end component CU_Decoder;
     
-    signal Instruction : STD_LOGIC_VECTOR (15 downto 0) := X"0000";
-    signal Reg1Read,  Reg2Read, RF_WHB, RF_WLB, RAM_Address_Src, RAM_Read, RAM_Write, JMP, JMP_Conditional, JMP_Relative, JMP_DestinationSource, Is_ALU_OP, Is_RAM_OP : STD_LOGIC;
-    signal Write_Data_sel : STD_LOGIC_VECTOR(1 downto 0);
+    signal instruction_s : std_logic_vector(15 downto 0) := X"0000";
     
     
     
 begin
 
+    -- Outputs are not connected, as they will be configured in the waveform
     EUT : CU_Decoder port map(
-        Instruction => Instruction,
-        Reg1Read => Reg1Read, 
-        Reg2Read => Reg2Read, 
-        RF_WHB => RF_WHB, 
-        RF_WLB => RF_WLB, 
-        Write_Data_Sel => Write_Data_Sel,  
-        RAM_Address_Src => RAM_Address_Src, 
-        RAM_Read => RAM_Read, 
-        RAM_Write => RAM_Write,   
-        JMP => JMP,  
-        JMP_Conditional => JMP_Conditional, 
-        JMP_Relative => JMP_Relative, 
-        JMP_DestinationSource => JMP_DestinationSource, 
-        Is_ALU_OP => Is_ALU_OP, 
-        Is_RAM_OP => Is_RAM_OP 
+        instructionToDecode => instruction_s
     );  
     
     
-    process is
+    simulator : process is
     begin
-        FOR i in 0 to 15 loop
-            Instruction(15 downto 12) <= std_logic_vector(to_unsigned(i, 4));
+        -- Iterates over all possible instructions, nothing else is used by the Control Unit
+        for i in 0 to 15 loop
+            instruction_s(15 downto 12) <= std_logic_vector(to_unsigned(i, 4));
             wait for 10ns;
         end loop;
-    end process;
+    end process simulator;
 
-end Behavioral;
+end architecture Behavioral;
--- vivado/ControlUnit/ControlUnit.srcs/sim_1/new/CU_RAMAddressController_SIM.vhd
+++ vivado/ControlUnit/ControlUnit.srcs/sim_1/new/CU_RAMAddressController_SIM.vhd
@@ -1,77 +1,55 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
 -- Create Date: 10.11.2024 16:23:48
--- Design Name: 
+-- Name: Lukas Reil
+-- Design Name: ShadeCpu
 -- Module Name: CU_RAMAddressController_SIM - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
+-- Project Name: ShadeCpu-1
+-- Target Devices: Arty A7-35T Development Board
+-- Repository: https://github.com/rtbnb/SixteenShadesOfCpu
 ----------------------------------------------------------------------------------
 
+library ieee;
+use ieee.std_logic_1164.ALL;
 
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity CU_RAMAddressController_SIM is
---  Port ( );
-end CU_RAMAddressController_SIM;
+end entity CU_RAMAddressController_SIM;
 
 architecture Behavioral of CU_RAMAddressController_SIM is
     component CU_RAMAddressController is
-        Port ( Reg2 : in STD_LOGIC_VECTOR (15 downto 0);
-               Immediate : in STD_LOGIC_VECTOR (15 downto 0);
-               MA : in STD_LOGIC_VECTOR (15 downto 0);
-               RAM_Address_Src : in STD_LOGIC;
-               Use_MA : in STD_LOGIC;
-               RAM_Address : out STD_LOGIC_VECTOR (15 downto 0));
+        Port (
+            reg2 : in std_logic_vector(15 downto 0);
+            immediate : in std_logic_vector(15 downto 0);
+            ramAddressSrc : in std_logic;
+            ramAddress : out std_logic_vector(15 downto 0)
+        );
     end component CU_RAMAddressController;
     
-    signal Reg2 : STD_LOGIC_VECTOR(15 downto 0) := X"0acb";
-    signal Immediate : STD_LOGIC_VECTOR(15 downto 0) := X"f123";
-    signal MA : STD_LOGIC_VECTOR(15 downto 0) := X"0007";
-    signal RAM_Address_Src, Use_MA : STD_LOGIC;
+    signal reg_2_s : std_logic_vector(15 downto 0) := X"0acb";
+    signal immediate_s : std_logic_vector(15 downto 0) := X"f123";
+    signal ram_address_src_s : std_logic;
 begin
+    -- Outputs are not connected, as they will be configured in the waveform
     EUT : CU_RAMAddressController port map(
-        Reg2 => Reg2,
-        Immediate => Immediate,
-        MA => MA,
-        RAM_Address_Src => RAM_Address_Src,
-        Use_MA => Use_MA
+        reg2 => reg_2_s,
+        immediate => immediate_s,
+        ramAddressSrc => ram_address_src_s
     );
     
-    process is
+    simulator : process is
     begin
-        RAM_Address_Src <= '0';
+        ram_address_src_s <= '0';
         Use_MA <= '0';
         wait for 10 ns;
-        RAM_Address_Src <= '0';
+        ram_address_src_s <= '0';
         Use_MA <= '1';
         wait for 10 ns;
-        RAM_Address_Src <= '1';
+        ram_address_src_s <= '1';
         Use_MA <= '0';
         wait for 10 ns;
-        RAM_Address_Src <= '1';
+        ram_address_src_s <= '1';
         Use_MA <= '1';
         wait for 10 ns;
-    end process;
+    end process simulator;
 
-end Behavioral;
+end architecture Behavioral;
