5d97ad430ed55835a2e5061109c6d3771aedf276
LukasReil <lukas_reil0305@gmx.net>
2024-12-04 08:55:30 +0100

    Updated Control Unit Simulations

--- vivado/ControlUnit/ControlUnit.srcs/sim_1/new/CU_ImmediateManipulator_SIM.vhd
+++ vivado/ControlUnit/ControlUnit.srcs/sim_1/new/CU_ImmediateManipulator_SIM.vhd
@@ -1,76 +1,60 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 10.11.2024 16:23:48
--- Design Name: 
--- Module Name: CU_ImmediateManipulator_SIM - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------
+-- -- Create Date: 10.11.2024 16:23:48
+-- -- Name: Lukas Reil
+-- -- Design Name: ShadeCpu
+-- -- Module Name: CU_ImmediateManipulator_SIM - Behavioral
+-- -- Project Name: ShadeCpu-1
+-- -- Target Devices: Arty A7-35T Development Board
+-- -- Repository: https://github.com/rtbnb/SixteenShadesOfCpu
+-- ----------------------------------------------------------------------------------
 
 
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
+library ieee;
+use ieee.std_logic_1164.ALL;
 
 entity CU_ImmediateManipulator_SIM is
---  Port ( );
-end CU_ImmediateManipulator_SIM;
+end entity CU_ImmediateManipulator_SIM;
 
 architecture Behavioral of CU_ImmediateManipulator_SIM is
+
     component CU_ImmediateManipulator is
-        Port ( Reg1 : in STD_LOGIC_VECTOR (15 downto 0);
-               Immediate : in STD_LOGIC_VECTOR (15 downto 0);
-               RF_WHB : in STD_LOGIC;
-               RF_WLB : in STD_LOGIC;
-               ManipulatedImmidiate : out STD_LOGIC_VECTOR (15 downto 0));
+        Port (
+            reg1 : in std_logic_vector(15 downto 0);
+            immediate : in std_logic_vector(15 downto 0);
+            rfWHB : in std_logic;
+            rfWLB : in std_logic;
+            manipulatedImmediate : out std_logic_vector(15 downto 0)
+        );
     end component CU_ImmediateManipulator;
     
-    signal Reg1 : std_logic_vector(15 downto 0) := X"abcd";
-    signal Immediate : std_logic_vector(15 downto 0) := X"3377";
-    signal RF_WHB, RF_WLB : STD_LOGIC;
+    signal reg1_s : std_logic_vector(15 downto 0) := X"abcd";
+    signal immediate_s : std_logic_vector(15 downto 0) := X"3377";
+    signal rf_whb_s, rd_wlb_s : std_logic;
     
 begin
+    -- Outputs are not connected, as they will be configured in the waveform
     EUT : CU_ImmediateManipulator port map(
-        Reg1 => Reg1,
-        Immediate => Immediate,
-        RF_WHB => RF_WHB,
-        RF_WLB => RF_WLB
+        reg1 => reg1_s,
+        immediate => immediate_s,
+        rfWHB => rf_whb_s,
+        rfWLB => rd_wlb_s
     );
     
-    
-    process is
+    -- Tests all possible combinations of rfWHB and rfWLB
+    simulator : process is
     begin
-        RF_WHB <= '0';
-        RF_WLB <= '0';
+        rf_whb_s <= '0';
+        rf_wlb_s <= '0';
         wait for 10 ns;
-        RF_WHB <= '0';
-        RF_WLB <= '1';
+        rf_whb_s <= '0';
+        rf_wlb_s <= '1';
         wait for 10 ns;
-        RF_WHB <= '1';
-        RF_WLB <= '0';
+        rf_whb_s <= '1';
+        rf_wlb_s <= '0';
         wait for 10 ns;
-        RF_WHB <= '1';
-        RF_WLB <= '1';
+        rf_whb_s <= '1';
+        rf_wlb_s <= '1';
         wait for 10 ns;
-    end process;
+    end process simulator;
 
-end Behavioral;
+end architecture Behavioral;
